508ac33ed6b3c8820a70ea5582b1a99da4e1b32c
Foux02 <94354146+Foux02@users.noreply.github.com>
2021-11-17 23:31:30 +0300

    StarShip

--- first/src/main/java/FirstTest.java
+++ first/src/main/java/FirstTest.java
@@ -1,5 +1,50 @@
-public  class FirstTest {
-	public static void main(String[] args){
-		System.out.println ("We are here! Hello!");
-	}
+import test.ship.StarShip;
+
+public class FirstTest {
+    public static void main(String[] args) {
+        System.out.println("We are here! Hello!");
+
+        System.out.println(8 + 2);
+        System.out.println(8 - 2);
+        System.out.println(8 * 2);
+        System.out.println(8 / 3);
+
+        System.out.println(8.0 / 3);
+
+        int side = 7;
+        System.out.println("Площадь квадрата со стороной " + side + " = " + (side * side));
+
+        double s = 7.45;
+        String text = "Площадь квадрата со стороной ";
+        System.out.println(text + s + " = " + (s * s));
+        helloTest();
+        helloTest("username");
+        System.out.println(helloTest("Значение 1","Зачение 2"));
+
+        //создаю экземпляр класса корабля
+        StarShip ship = new StarShip(253.25);
+        displayHeight (ship);
+
+    }
+
+    /*демо методов*/
+    //1 метод без параметров
+    public static void helloTest() {
+        System.out.println("We are here! Hello!");
+    }
+
+    //2 метод с параметром
+    public static void helloTest(String text) {
+        System.out.println("We are here! Hello! "+ text);
+    }
+
+    //3 метод с возвратом значения
+    public static String helloTest(String value1, String value2) {
+        return "We are here! Hello! "+ value1 +" , "+ value2;
+    }
+    public static void displayHeight (StarShip ship) {
+        System.out.println("Высота корабля составляет : "+ ship.getShipHeight());
+    }
+
 }
+
---
+++ first/src/main/java/test/ship/StarShip.java
@@ -0,0 +1,18 @@
+package test.ship;
+    /*Created by Jane 17 11 2021*/
+
+    public class StarShip {
+        /*высота*/
+        public double shipHeight;
+
+        //добавить конструктор для кораблика
+        public StarShip(double shipHeight) {
+            this.shipHeight = shipHeight;
+        }
+
+            //возвращает высоту коробля
+        public double getShipHeight() {
+            return this.shipHeight;
+            }
+
+}
